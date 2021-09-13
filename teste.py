e0e75718419dbf64549baa5fca408c2ce581eb77
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2021-09-13 18:57:37 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,20 @@
+print("Hello")
+soma = 2 + 5
+print(soma)
+mult = 2 * 3
+print(mult)
+print("testando")
+import math
+print(math.ceil(3.4))
+import pandas as pd
+x = pd.read_excel(r"C:\Users\jeann\Documents\Python\exe_texture_ggplot2.xlsx") # Importar tabela
+print(x) # Ler tabela
+texto = 'machine learning python IESB'
+print(texto.lower())
+print(texto.upper())
+print(len(texto))
+print(x.shape)
+print(x.head())
+print(x.tail())
+print(x['germinacao'])
+print(x['germinacao'].describe())
